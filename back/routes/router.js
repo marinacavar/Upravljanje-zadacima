2bc6e2951c786dfdf8848eb9c3761b3eac951cca
marinacavar <marinacavar>
2024-03-28 09:16:59 +0100

    -

--- back/routes/router.js
+++ back/routes/router.js
@@ -1,39 +1,18 @@
 const express = require('express');
 const route = express.Router();
 const controller = require('../controller/user');
-const axios = require('axios');
+const controllers = require('../controller/task');
 
-route.get('/', (req,res) => {
-    axios.get('http://localhost:3001/api/users')
-    .then(function(response){
-        console.log(response.data)
-        res.render('index',{users: response.data});
 
-    })
-    .catch(err=>{
-        res.send(err);
-    })
-    
-})
-
-route.get('/add-user', (req,res) => {
-    res.render('add_user');
-})
-
-route.get('/update-user', (req,res) => {
-    axios.get("http://localhost:3001/api/users",{params: {id:req.query.id}})
-    .then(function(userdata){
-        res.render("update_user",{user:userdata.data})
-    })
-    .catch(err => {
-        res.send(err);
-    })
-})
 route.post('/api/users',controller.create);
-// Add route for login
 route.post('/login', controller.login);
 route.get('/api/users',controller.find);
 route.put('/api/users/:id',controller.update);
 route.delete('/api/users/:id', controller.delete);
+
+route.post('/task',controllers.create);
+route.get('/task',controllers.find);
+route.put('/task/:id',controllers.update);
+route.delete('/task/:id', controllers.delete);
 
 module.exports=route;
